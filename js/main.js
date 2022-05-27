c7027423d4cd5b12af0f3ce7dd4409734eeaa6d0
Ryan DaBrusco <71036099+rdabrusco@users.noreply.github.com>
2022-05-27 11:09:11 -0400

    add a button that chooses a random item and returns it.  hide other page values until item is chosen. add whichItem to main.js, randomItem params to server

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 document.querySelector('#clickMe').addEventListener('click', makeReq);
-let clownLaugh = new Audio('(clown-laughing)-cqp');
+document.querySelector('#random').addEventListener('click', whichItem)
+// let audio = document.querySelector('#laugh')
 
 async function makeReq() {
   const guess = document.querySelector('#guess').value;
@@ -7,9 +8,31 @@ async function makeReq() {
   const data = await res.json();
 
   console.log(data);
-  if (data.verdict === 'The Price is WRONG!') {
-    clownLaugh.play();
-    clownLaugh.loop = true;
-  }
+  // if (data.verdict === 'The Price is Wrong!!!! Too low!' || data.verdict === "The Price is WRONG! too High!") {
+  //    audio.play()
+    
+  // }
   document.querySelector('#rightOrWrong').textContent = data.verdict;
+}
+// Picks a random item to then guess the price of, and displays the item picture and name.
+async function whichItem(){
+  const title = document.querySelector('#itemTitle')
+  // Makes an array of elements with class hidden
+  const hidden = document.querySelectorAll('.hidden')
+  
+
+
+
+  // call to api
+  const res = await fetch('/api?randomItem=true')
+  //resolve to json
+  const data = await res.json()
+  console.log(data)
+  // removes hidden class from attributes that have it
+  hidden.forEach(e => e.classList.toggle('hidden'))
+  // hide random item button
+  document.querySelector('#random').classList.add('hidden')
+  title.innerText += ` ${data._name}?`
+
+
 }
--- server.js
+++ server.js
@@ -4,11 +4,36 @@ const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet');
 
+class Item{
+  constructor(name, price, img){
+    this._name = name
+    this._price = price
+    this.img = img
+  }
+  getName(){
+    return this.name
+  }
+  getPrice(){
+    return this.price
+  }
+  getImg(){
+    return this.img
+  }
+}
+let tesla = new Item('Tesla', 45000, '/img/t.jpeg')
+let kaid = new Item('Kitchen Aid', 450, '/img/kaid.jpeg')
+
+let items = [tesla, kaid]
+
+ 
+
 const server = http.createServer((req, res) => {
   const page = url.parse(req.url).pathname;
   const params = querystring.parse(url.parse(req.url).query);
   console.log(params);
   console.log(page);
+  
+  
 
   const reWrite = (file, contentType) => {
     fs.readFile(file, function (err, data) {
@@ -20,14 +45,26 @@ const server = http.createServer((req, res) => {
   if (page == '/') {
     reWrite('index.html', 'text/html');
   } else if (page == '/api') {
+
+    // When clikcing randomItem, returns a random item from list of items, and that is the item the game is played with.
+     if (params['randomItem'] == 'true') {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      //Chooses item
+      let item = (items[Math.floor(Math.random()*2)])
+      console.log(item)
+      // returns item as json object
+      res.end(JSON.stringify(item));
+    }
+      // Compares given price to selected item price
     if (params['priceIsWrong'] == '') {
       res.writeHead(200, { 'Content-Type': 'application/json' });
       const objToJson = {
+        
         verdict: 'Enter a value!!!',
       };
       res.end(JSON.stringify(objToJson));
     }
-    if (Number(params['priceIsWrong']) < 45000) {
+    else if (Number(params['priceIsWrong']) < 45000) {
       res.writeHead(200, { 'Content-Type': 'application/json' });
       const objToJson = {
         verdict: 'The Price is Wrong!!!! Too low!',
@@ -47,6 +84,7 @@ const server = http.createServer((req, res) => {
       };
       res.end(JSON.stringify(objToJson));
     }
+    
     //priceIsWrong if
   } //else if
   else if (page == '/css/style.css') {
